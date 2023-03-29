de628382359250b49d3d032c9de984326a2e9066
mutugi-c <mutugichris@gmail.com>
2023-03-29 19:44:29 +0200

    Create and style a to do list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import "./styles.css";
+import './styles.css';
 
 const tasksArr = [
   {
-    description: "Write code",
+    description: 'Write code',
     completed: false,
     index: 0,
   },
   {
-    description: "Watch football",
+    description: 'Watch football',
     completed: false,
     index: 1,
   },
   {
-    description: "Sleep",
+    description: 'Sleep',
     completed: true,
     index: 2,
   },
 ];
+
+const toDoList = document.getElementById('to-do-list');
+
+function populateTaskList() {
+  tasksArr.forEach((task) => {
+    const listItem = document.createElement('li');
+    li.text;
+  });
+}
