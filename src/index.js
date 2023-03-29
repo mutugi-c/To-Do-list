5e118897dbe7e479b3afbedd6d6a65758870e278
mutugi-c <mutugichris@gmail.com>
2023-03-29 21:20:29 +0200

    Add tasks to the list dynamically

--- src/index.js
+++ src/index.js
@@ -20,9 +20,19 @@ const tasksArr = [
 
 const toDoList = document.getElementById('to-do-list');
 
-function populateTaskList() {
-  tasksArr.forEach((task) => {
-    const listItem = document.createElement('li');
-    li.text;
+function populateTaskList(arr) {
+  arr.forEach((task) => {
+    const toDoItem = document.createElement('li');
+    toDoItem.classList.add('to-do-item');
+    toDoItem.innerHTML = `
+    <button class="check-button">
+      <span class="fas fa-check"></span>
+    </button>
+    <span class="task-description">${task.description}</span>
+    <span class="fas fa-ellipsis-v"></span>
+    `;
+    toDoList.appendChild(toDoItem);
   });
 }
+
+populateTaskList(tasksArr);
