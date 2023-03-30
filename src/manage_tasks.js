4fd348e9f861d262c9840abee052489e480ceec0
mutugi-c <mutugichris@gmail.com>
2023-03-30 19:09:31 +0200

    Create a method to add tasks

--- src/manage_tasks.js
+++ src/manage_tasks.js
@@ -4,6 +4,15 @@ class ManageTasks {
   constructor(taskArr) {
     this.taskArr = taskArr;
   }
+
+  addTask(taskDescrption) {
+    const newTask = {
+      description: taskDescrption,
+      completed: false,
+      index: taskArr.length,
+    }
+    taskArr.push(newTask);
+  }
 }
 
 export default ManageTasks;
