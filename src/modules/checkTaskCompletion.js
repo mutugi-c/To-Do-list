d04fd9f1c22ebf1d38bc42dc96bff835e121f1d9
mutugi-c <mutugichris@gmail.com>
2023-03-31 19:24:23 +0200

    Create update status and remove task functions

--- src/modules/checkTaskCompletion.js
+++ src/modules/checkTaskCompletion.js
@@ -1,9 +1,27 @@
 // Update completed status
 const toggleCompleted = () => {
-
-}
+  const toDoItems = toDoList.querySelectorAll('.to-do-item');
+  toDoItems.forEach((toDoItem) => {
+    const checkButton = toDoItem.querySelector('.check-button');
+    if (checkButton.checked) {
+      toDoItem.classList.add('completed');
+    } else {
+      toDoItem.classList.remove('completed');
+    }
+  });
+};
 
 // Remove tasks identified as completed
 const clearCompleted = () => {
+  const toDoItems = toDoList.querySelectorAll('.to-do-item');
+  toDoItems.forEach((toDoItem) => {
+    const checkButton = toDoItem.querySelector('.check-button');
+    if (checkButton.checked) {
+      const index = toDoItem.dataset.index - 1;
+      manageTasks.removeTask(index);
+      toDoItem.remove();
+    }
+  });
+};
 
-}
+export { toggleCompleted, clearCompleted };
